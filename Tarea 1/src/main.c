01f52e0c3027d9b5d6079e00992173cf68945f27
segonzal <seba-5@hotmail.com>
2015-10-10 18:03:08 -0300

    Agrega seleccion aleatoria de patron de tamaño dado

--- Tarea 1/src/main.c
+++ Tarea 1/src/main.c
@@ -8,6 +8,8 @@
 #define MAX_PAT_EXP 7
 #define MAX_TEXT_SIZE 1000000
 
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 /*==============================================*/
 
 FILE *openLog(const char* name){
@@ -21,7 +23,7 @@ FILE *openLog(const char* name){
 
 /*==============================================*/
 
-void loadBuffer(char *filename,char* buffer){
+int loadBuffer(char *filename,char* buffer){
   FILE *in;
   size_t read;
   in = fopen(filename, "r");
@@ -40,13 +42,13 @@ void loadBuffer(char *filename,char* buffer){
   for (size_t i = read; i <= MAX_TEXT_SIZE+1; i++) {
     buffer[i] = 0;
   }
-
   fclose(in);
+  return read;
 }
 
 /*==============================================*/
 
-int comparison_counter, start_time, end_time, iteration, pattern_length;
+int comparison_counter, start_time, end_time, iteration, pattern_length, text_length;
 /*filenames and algorithm name*/
 char *test_algorithm;
 char *test_dataset;
@@ -60,12 +62,21 @@ void configureTest(char *algorithm,char *dataset){
   test_algorithm = algorithm;
   test_dataset = dataset;
   /*open the dataset and load to memory*/
-  loadBuffer(dataset,text_buffer);
+  text_length = loadBuffer(dataset,text_buffer);
 }
 
 void choosePattern(int i){
+  int r;
   pattern_length = 1<<i;
   /*load the pattern to buffer*/
+  if (text_length<pattern_length){
+    printf("Error: Pattern to large for text\n");
+    exit(3);
+  }
+  r = rand() % MAX(1,text_length-pattern_length);
+  for (size_t i = 0;  i<(1<<MAX_PAT_EXP)+1; i++) {
+    pattern_buffer[i] = i<pattern_length? text_buffer[r+i] : 0;
+  }
 }
 
 void beginTest(){
@@ -120,6 +131,9 @@ int main(int argc, char const *argv[]){
   char* file_name, test_name;
   FILE *f;
 
+  /*setting the seed for random*/
+  srand(time(NULL));
+
   /*Allocate ONCE the text working buffer*/
   text_buffer = (char*) malloc(sizeof(char)*(MAX_TEXT_SIZE+1));
   if (text_buffer == NULL) {printf("Failed to allocate text memory!\n");exit(2);}
@@ -128,26 +142,26 @@ int main(int argc, char const *argv[]){
 
   f = openLog("results.csv");
   fprintf(f, "ALG\tDATA SET\tPLEN\tIT\tEJECTIME\tNCOM\n");
-  /*---BINARY---*/
+  /*---BINARY---*//*
   runTest("BF","dataset/BINA.txt",f,&funBF);
   runTest("KMP","dataset/BINA.txt",f,&funKMP);
-  runTest("BMH","dataset/BINA.txt",f,&funBMH);
-  /*---REAL DNA---*/
+  runTest("BMH","dataset/BINA.txt",f,&funBMH);*/
+  /*---REAL DNA---*//*
   runTest("BF","dataset/RDNA.txt",f,&funBF);
   runTest("KMP","dataset/RDNA.txt",f,&funKMP);
-  runTest("BMH","dataset/RDNA.txt",f,&funBMH);
+  runTest("BMH","dataset/RDNA.txt",f,&funBMH);*/
   /*---SYNTH DNA---*/
-  runTest("BF","dataset/SDNA.txt",f,&funBF);
+  runTest("BF","dataset/SDNA.txt",f,&funBF);/*
   runTest("KMP","dataset/SDNA.txt",f,&funKMP);
-  runTest("BMH","dataset/SDNA.txt",f,&funBMH);
-  /*---REAL LNG---*/
+  runTest("BMH","dataset/SDNA.txt",f,&funBMH);*/
+  /*---REAL LNG---*//*
   runTest("BF","dataset/RLNG.txt",f,&funBF);
   runTest("KMP","dataset/RLNG.txt",f,&funKMP);
-  runTest("BMH","dataset/RLNG.txt",f,&funBMH);
-  /*---SYNTH LNG---*/
+  runTest("BMH","dataset/RLNG.txt",f,&funBMH);*/
+  /*---SYNTH LNG---*//*
   runTest("BF","dataset/SLNG.txt",f,&funBF);
   runTest("KMP","dataset/SLNG.txt",f,&funKMP);
-  runTest("BMH","dataset/SLNG.txt",f,&funBMH);
+  runTest("BMH","dataset/SLNG.txt",f,&funBMH);*/
 
   fclose(f);
 
