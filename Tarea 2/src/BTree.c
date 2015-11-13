8f09e9c78e68284deb8cbaec758f6dae92a385fe
Sebastian Gonzalez <seba-5@hotmail.com>
2015-11-13 14:03:57 -0300

    Add mockup of BTree

--- Tarea 2/src/BTree.c
+++ Tarea 2/src/BTree.c
@@ -1,1 +1,51 @@
 #include "BTree.h"
+
+BTree CreateBTree(){
+  Btree T = (BTree) malloc(sizeof(*T));
+  BTNode x = (BTNode) malloc(sizeof(*x));
+  x->LEAF = 1;
+  x->n = 0;
+  /*diskwrite(x)*/
+  T->root = x;
+  return T;
+}
+
+int insert(BTree T,int k){
+  BTNode r = T->root;
+  if (r->n == 2*t-1){/*TODO: define t*/
+    BTNode s = (BTNode) malloc(sizeof(*s));
+    T->root = s;
+    s->LEAF = 0;
+    s->n = 0;
+    s->pointers[0] = r;
+    split_child(T,s,1,r);
+    insert_nonfull(T,s,k);
+  }
+  else insert_nonfull(T,r,k);
+}
+
+void split_child(BTree T,BTNode x,int i,BTNode y){
+}
+
+void insert_nonfull(BTree T,BTNode x,int k){
+  int i = x->n;/*n-1?*/
+  if (x->LEAF){
+    while (i>=0 && k < x->key[i]){
+      x->key[i+1]=x->key[i];
+      i--;
+    }
+    x->key[i+1]=k;
+    x->n++;
+    /*diskwrite(x)*/
+  }
+  else {
+    while (i>=0 && k < x->key[i]) i--;
+    i++
+    /*y = diskread(x->pointers[i])*/
+    if (y->n==2*t-1){
+      split_child(x,i,y);
+      if (k>x->key[i]) i++;
+      insert_nonfull(y,k);
+    }
+  }
+}
--- Tarea 2/src/BTree.h
+++ Tarea 2/src/BTree.h
@@ -6,5 +6,21 @@
 extern int debug;
 extern int blocks_written, blocks_read;
 
+typedef struct btnode {
+  int n; /*numero de llaves que contiene*/
+  int key[]; /*llaves [n]*/
+  int LEAF; /*valor booleano is_LEAF?*/
+  int pointers[]; /*si es falso que LEAF, entonces guardo punteros [n+1]*/
+} *BTNode;
+
+typedef struct {
+  BTNode root;
+} *BTree;
+
+BTree CreateBTree();
+
+int insert(BTree T,char *str);
+void split_child(BTree T,BTNode s,int i,BTNode k);
+void insert_nonfull(BTree T,BTNode x,int k);
 
 #endif
