ff276c2bc9c485d890056a943b1e76ba4ef7d3b8
Sebastian Gonzalez <seba-5@hotmail.com>
2015-11-13 14:03:18 -0300

    Add hash function for test case

--- Tarea 2/src/main.h
+++ Tarea 2/src/main.h
@@ -4,4 +4,20 @@
 int debug = 0;
 int blocks_written,blocks_read;
 
+/*Returns the hash of a 15-long DNA sequence*/
+int MakeHashGCAT(char *str){
+  int mask,result;
+  result = 0;
+  for (size_t i = 0; i < 15; i++) {
+    switch (str[i]) {
+      case "C": mask = 0; break;
+      case "G": mask = 1; break;
+      case "A": mask = 2; break;
+      case "T": mask = 3; break;
+    }
+    result |= mask<<i;
+  }
+  return result;
+}
+
 #endif
