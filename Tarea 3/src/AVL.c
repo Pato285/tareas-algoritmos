b20a0bf2d3c4acdf54c499e39b981aaf42d17e98
segonzal <seba-5@hotmail.com>
2015-12-05 17:11:34 -0300

    Fixed AVL. It may require more test to check everything is Ok.

--- Tarea 3/src/AVL.c
+++ Tarea 3/src/AVL.c
@@ -67,7 +67,6 @@ TNode doubleLeftRotation(TNode node){
 }
 
 int isAVL(TNode node){
-	if (node->height <= 1) return TRUE;
 	return abs(height(node->right) - height(node->left)) <= 1;
 }
 
@@ -96,7 +95,24 @@ TNode insertAVL(TNode node,char key[],char value[]){
 	return node;
 }
 
-TNode findPredecessorParentAVL(TNode node,char key[]){
+TNode checkAVLDelete(TNode node){
+	if ( isAVL(node) ) return node;
+	TNode y = height(node->left)>height(node->right) ? node->left : node->right;
+	TNode x = height(y->left)>height(y->right) ? y->left : y->right;
+	TNode r;
+	if ( y == node->left) {
+		if ( x == y->left ) r = leftRotation(node);
+		else if ( x == y->right ) r = doubleLeftRotation(node);
+	}
+	else if ( y == node->right) {
+		if ( x == y->right ) r = rightRotation(node);
+		else if ( x == y->left ) r = doubleRightRotation(node);
+	}
+	r->height = height(r);
+	return r;
+}
+
+TNode findPredecessorParentAVL(TNode node){
 	TNode k;
 	if (node->right->right == NULL){
 		k = node->right;
@@ -104,14 +120,9 @@ TNode findPredecessorParentAVL(TNode node,char key[]){
 		k->right = NULL;
 		k->left = NULL;
 	}
-	else k = findPredecessorParentAVL(node->right,key);
+	else k = findPredecessorParentAVL(node->right);
+	node = checkAVLDelete(node);
 	node->height = height(node);
-	if ( !isAVL(node) ) {
-		if ( strcmp(node->right->key,key)>0 )
-			node = doubleRightRotation(node);
-		else
-			node = rightRotation(node);
-	}
 	return k;
 }
 
@@ -127,31 +138,39 @@ TNode deleteAVL(TNode node,char key[]){
 		}
 		else if(node->right == NULL){
 			r = node->left;
+			node->left = NULL;
 		}
 		else if(node->left == NULL){
 			r = node->right;
+			node->right = NULL;
 		}
 		else if (node->left->right == NULL){
 			r = node->left;
 			r->right = node->right;
+			node->left =  NULL;
+			node->right = NULL;
 		}
 		else {
-			r = findPredecessorParentAVL(node->left,key);
+			r = findPredecessorParentAVL(node->left);
 			r->right = node->right;
 			r->left = node->left;
+			node->left =  NULL;
+			node->right = NULL;
 		}
-		node->left =  NULL;
-		node->right = NULL;
 		freeTNode(node);
 		free(node);
+		/*CHECK AVL CONDITION*/
+		r = checkAVLDelete(r);
 		r->height = height(r);
 		return r;
 	}
 	else if (s>0) node->left = deleteAVL(node->left,key);
 	else node->right = deleteAVL(node->right,key);
+	node = checkAVLDelete(node);
 	node->height = height(node);
 	return node;
 }
+
 int searchAVL(TNode node,char key[]){
 	if (node == NULL) return FALSE;
 
